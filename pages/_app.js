fee5c511957e31de30ea0ee9627750bf70987ff9
luisfe97 <luisfelipegomezr@hotmail.com>
2023-04-15 00:29:42 -0500

    3

--- pages/_app.js
+++ pages/_app.js
@@ -19,11 +19,7 @@ export default function App({ Component, pageProps }) {
         <meta content name="keywords" />
         {/* Favicons */}
         {/* Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
         {/* Vendor CSS Files */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
       </Head>
       <MoralisProvider initializeOnMount={false}>
         <Gsapscroll />
